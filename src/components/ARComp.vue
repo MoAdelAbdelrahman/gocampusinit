44f85ef5c30bcf1da03a2b7595df4050af87820e
MoAdelAbdelrahman <mahmd.3adel@gmail.com>
2024-06-06 13:10:08 -0600

    fuck ar

--- src/components/ARComp.vue
+++ src/components/ARComp.vue
@@ -1,31 +1,39 @@
 <template>
   <div v-if="showAr" class="ar-container">
     <a-scene
-      vr-mode-ui="enabled: false"
-      embedded
-      arjs="sourceType: webcam; debugUIEnabled: false;"
+      vr-mode-ui="enabled: false;"
+      arjs="sourceType: webcam; debugUIEnabled: false; patternRatio: 0.70"
+      renderer="logarithmicDepthBuffer: true"
+      device-orientation-permission-ui="enabled: true"
     >
-      <a-assets>
-        <a-asset-item id="model" src="./3dmodels/monitor4.gltf"></a-asset-item>
-      </a-assets>
-      <a-entity
-        v-if="latitude && longitude"
-        gps-entity-place="latitude: {{latitude}}; longitude: {{longitude}};"
-        gltf-model="#model"
-        scale="0.5 0.5 0.5"
-        animation-mixer
-      ></a-entity>
+      <!-- Marker-based AR entity -->
+      <a-marker type="pattern" url="https://cdn.glitch.me/d975da3b-49e7-49c4-b6a6-e9b8599fb63f%2Fpattern-fly.patt">
+        <a-entity
+          id="bot"
+          gltf-model="url(https://cdn.glitch.me/fc511399-d148-4898-ad51-f0b6fafd32a6/scene.gltf)"
+          animation-mixer="clip: *;"
+          visible="true"
+          scale="30 30 30"
+          position="0 0 -1"
+          rotation="-90 0 0"
+        ></a-entity>
+      </a-marker>
+      
+      <!-- Location-based text -->
       <a-text
         v-if="latitude && longitude"
+        id="gps-text"
         value="This content will always face you."
         look-at="[gps-camera]"
         scale="50 50 50"
-        gps-entity-place="latitude: {{latitude}}; longitude: {{longitude}};"
+        :gps-entity-place="`latitude: ${latitude}; longitude: ${longitude};`"
       ></a-text>
+      
       <a-camera gps-camera rotation-reader></a-camera>
     </a-scene>
   </div>
 </template>
+
 
 
 
@@ -59,7 +67,7 @@ export default {
     setPosition(position) {
       this.latitude = position.coords.latitude;
       this.longitude = position.coords.longitude;
-      this.updateModelPosition();
+      this.$nextTick(this.updateTextPosition); // Ensure DOM update before calling the method
     },
     showError(error) {
       switch (error.code) {
@@ -77,35 +85,41 @@ export default {
           break;
       }
     },
-    updateModelPosition() {
-      const modelEntity = document.querySelector('[gps-entity-place]');
-      if (modelEntity) {
-        modelEntity.setAttribute('gps-entity-place', `latitude: ${this.latitude}; longitude: ${this.longitude};`);
+    updateTextPosition() {
+      const textEntity = document.querySelector('#gps-text');
+      if (textEntity) {
+        textEntity.setAttribute('gps-entity-place', `latitude: ${this.latitude}; longitude: ${this.longitude};`);
+      } else {
+        console.error('Text entity not found.');
       }
     }
   },
   watch: {
     showAr(newVal) {
       if (newVal) {
-        this.updateModelPosition();
+        this.$nextTick(this.updateTextPosition); // Ensure DOM update before calling the method
       }
     },
     latitude() {
-      this.updateModelPosition();
+      this.$nextTick(this.updateTextPosition); // Ensure DOM update before calling the method
     },
     longitude() {
-      this.updateModelPosition();
+      this.$nextTick(this.updateTextPosition); // Ensure DOM update before calling the method
     }
   }
 };
 
 
+
 </script>
 
 <style>
+
 .ar-container {
   position: relative;
   width: 100%;
   height: 80vh; /* 80% of the viewport height */
 }
+
+
 </style>
